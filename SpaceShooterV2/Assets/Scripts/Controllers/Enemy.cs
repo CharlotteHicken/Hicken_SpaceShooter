f5cd7de19f6df9532b323ecaaf5a3375676c9a81
CharlotteHicken <hickecha@sheridancollege.ca>
2024-09-22 17:29:05 -0400

    Created an Enemy Movement system

--- SpaceShooterV2/Assets/Scripts/Controllers/Enemy.cs
+++ SpaceShooterV2/Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public float maxSpeed;
+    float acceleration;
+    public float accelerationRate;
+    public Transform player;
+
+    private void Start()
+    {
+        acceleration = maxSpeed / accelerationRate;
+    }
     private void Update()
     {
+        MoveToPlayer();
+    }
+
+    void MoveToPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+
+        transform.position += direction * acceleration * Time.deltaTime;
     }
 
 }
