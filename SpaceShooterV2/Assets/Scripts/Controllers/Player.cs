9d9cdd842a16991394599bed95e079fa709abdab
CharlotteHicken <hickecha@sheridancollege.ca>
2024-09-22 15:19:42 -0400

    Added acceleration and deceleration to the player's movement

--- SpaceShooterV2/Assets/Scripts/Controllers/Player.cs
+++ SpaceShooterV2/Assets/Scripts/Controllers/Player.cs
@@ -9,7 +9,19 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-    public float speed;
+    Vector3 velocity;
+
+    float acceleration;
+    float deceleration;
+    public float maxSpeed;
+    public float accelerateTime;
+    public float decelerateTime;
+
+    private void Start()
+    {
+        acceleration = maxSpeed / accelerateTime;
+        deceleration = maxSpeed / decelerateTime;
+    }
 
     void Update()
     {
@@ -20,8 +32,25 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 velocity = new Vector3(horizontalInput * speed, verticalInput * speed, 0);
-        transform.position = transform.position + velocity * Time.deltaTime;
+        Vector3 inputValues = new Vector3(horizontalInput, verticalInput, 0);
+
+        if (inputValues.magnitude != 0)
+        {
+            velocity += inputValues * acceleration * Time.deltaTime;
+        }
+        else
+        {
+            velocity -= velocity * deceleration * Time.deltaTime;
+        }
+
+        if (velocity.magnitude < 0.001f)
+        {
+            velocity = Vector3.zero;
+        }
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        transform.position += velocity * Time.deltaTime;
     }
 
 }
