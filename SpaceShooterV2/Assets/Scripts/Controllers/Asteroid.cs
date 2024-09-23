a0e2c7f9ee19838be88c3deaaccfd88a075ac156
CharlotteHicken <hickecha@sheridancollege.ca>
2024-09-23 11:39:07 -0400

    Asteroids move

--- SpaceShooterV2/Assets/Scripts/Controllers/Asteroid.cs
+++ SpaceShooterV2/Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,37 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    Vector3 destinationGoal;
+
     // Start is called before the first frame update
     void Start()
     {
+        SetDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+        transform.position = Vector3.Lerp(transform.position, destinationGoal, moveSpeed * Time.deltaTime);
+
+        float almostThere = (transform.position - destinationGoal).magnitude;
+
+        if (almostThere < arrivalDistance)
+        {
+            SetDestination();
+        }
+    }
+
+    public void SetDestination()
+    {
+        float xValue = Random.Range(-maxFloatDistance, maxFloatDistance);
+        float yValue = Random.Range(-maxFloatDistance, maxFloatDistance);
+
+        destinationGoal = new Vector3(transform.position.x + xValue, transform.position.y + yValue, 0);
     }
 }
