1393ca082407395d7d541cf802994853d97dd39b
CharlotteHicken <hickecha@sheridancollege.ca>
2024-09-23 12:23:53 -0400

    Created a constellation line drawer

--- SpaceShooterV2/Assets/Scripts/Controllers/Stars.cs
+++ SpaceShooterV2/Assets/Scripts/Controllers/Stars.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
-    public float drawingTime;
+    public int interpolationFrameCount;
+    int elapsedFrames = 0;
+    float drawingTime;
+    int currentStarList = 0;
 
     // Update is called once per frame
     void Update()
     {
+        drawingTime = (float)elapsedFrames / interpolationFrameCount;
+
+            Vector3 currentStar = starTransforms[currentStarList].position;
+            Vector3 nextStar = starTransforms[currentStarList + 1].position;
+            Vector3 interpolatedPosition = Vector3.Lerp(currentStar, nextStar, drawingTime);
+            Debug.DrawLine(currentStar, interpolatedPosition, Color.white);
+
+        elapsedFrames++;
+         
+        if (elapsedFrames > interpolationFrameCount)
+        {
+            currentStarList++;
+            elapsedFrames = 0;
+
+            if (currentStarList >= starTransforms.Count - 1)
+            {
+                currentStarList = 0;
+            }
+        
+        }
     }
 }
