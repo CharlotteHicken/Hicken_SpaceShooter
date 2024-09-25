2c01f9b8971b17b721294056f9a264b4ad158c99
CharlotteHicken <hickecha@sheridancollege.ca>
2024-09-25 18:08:12 -0400

    Created an orbital system

--- SpaceShooterV2/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooterV2/Assets/Scripts/Controllers/Moon.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float orbitRadius;
+    public float orbitSpeed;
+    float currentAngle = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(orbitRadius, orbitSpeed, planetTransform);
+    }
+
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        currentAngle += speed * Time.deltaTime;
+        Vector3 movePosition = new Vector3(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle)) * radius;
+
+        transform.position = movePosition + target.position;
 
     }
 }
